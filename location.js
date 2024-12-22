adbc2b9ed79a9b7c4c7f7c43eaa1299d9f68c589
IAMRKG <radhacreative@gmail.com>
2024-12-22 14:09:45 +0530

    Revert "Merge pull request #2 from IamRKG/basic-expand"
    This reverts commit a8a3d5e23fc106118d740dd8a7a61e1686f74b8e, reversing
    changes made to 35f9c586458b0f4d69726bee22b9e29c2ab7d255.

--- location.js
+++ location.js
@@ -34,61 +34,27 @@ LOCATION_MAP.forEach((country) => {
   const elementLI = document.createElement('li');
   elementLI.classList.add('country-item');
   elementLI.textContent = country.countryName;
-
-
-
+  
   const elementULState = document.createElement('ul');
   const elementULCity = document.createElement('ul');
   elementULState.classList.add('state-list');
-  elementULCity.classList.add('city-list');
 
   country.children.forEach((state) => {
     const elementLIState = document.createElement('li');
     elementLIState.textContent = state.stateName;
-    elementLIState.className = 'state-item'
     elementULState.appendChild(elementLIState);
-    // Set initial display state
-    elementULState.style.display = 'none';
-    elementULCity.style.display = 'none';
-
-    // Single event handler for country
-    elementLI.addEventListener('click', function(event) {
-      if (event.target === this) {
-        toggleList(elementULState);
-       // elementULState.style.display = elementULState.style.display === 'none' ? 'block' : 'none';
-        event.stopPropagation();
-      }
-    });
 
     state.children.forEach((city) => {
       const elementLICity = document.createElement('li');
       elementLICity.textContent = city.cityName;
-      elementLICity.className = 'city-item'
       elementULCity.appendChild(elementLICity);
-
-      // Single event handler for state
-      elementLIState.addEventListener('click', function(event) {
-        if (event.target === this) {
-        //  elementULCity.style.display = elementULCity.style.display === 'none' ? 'block' : 'none';
-          toggleList(elementULCity);
-          event.stopPropagation();
-        }
-      });
     });
   });
 
   elementLI.appendChild(elementULState);
   elementULState.appendChild(elementULCity);
-  // elementULState.style.display = 'none'
   elementUL.appendChild(elementLI);
-
 });
 
 fragment.appendChild(elementUL);
 document.body.appendChild(fragment);
-
-const toggleList = (listElement) => {
-  const isExpanded = listElement.style.display === 'block';
-  listElement.style.display = isExpanded ? 'none' : 'block';
- // arrow.parentElement.classList.toggle('expanded');
-};
